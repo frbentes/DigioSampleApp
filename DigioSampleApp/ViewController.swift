570155f2c85ad3f1eb5b92d4bd5c363809b19b03
Fredyson Bentes <fredyson.costa@vortigo.com.br>
2024-08-30 13:24:16 -0400

    Initial Commit

---
+++ DigioSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigioSampleApp
+//
+//  Created by Fredyson Costa on 30/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
